fa0d1dd7be7b76ea6319647ab966e4c0da40c651
erazomd <43145313+erazomd@users.noreply.github.com>
2018-09-10 11:51:15 -0500

    edicion
    edicion full

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
 print("hello")
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+test = get_destination_index("Cairo, Egypt")
+print (test)
